cc02c0891e3b5ef60ff0ef387e3338bed06b8adf
loserStar <xinxin321198@gmail.com>
2017-02-13 15:10:13 +0800

    Update setPath.bat
    add maven

--- gradleWebProject/setPath.bat
+++ gradleWebProject/setPath.bat
@@ -4,11 +4,10 @@ chcp 936
 rem @setlocal
 set JAVA_HOME=e:\jdk1.8
 set SVN_HOME=c:\Program Files\TortoiseSVN
-set MYSQL_HOME=e:\ynzl\bin\MySql
-set GRADLE_HOME=e:\ynzl\bin\gradle
-set GRADLE_USER_HOME=e:\ynzl\tmp\.gradle\
+set GRADLE_HOME=E:\gradle3.3
+set MAVEN_HOME=E:\maven3.3.9
 set classpath=.;.\classes\;.\target\test-classes\;.\target\classes\;%JAVA_HOME%\jre\lib\rt.jar;%JAVA_HOME%\lib\tools.jar;
 set APR_ICONV_PATH=%SVN_HOME%\iconv
 set LANG=zh_CN.UTF8
-set path=.;%JAVA_HOME%\bin;%GRADLE_HOME%\bin;%SVN_HOME%\bin;%MYSQL_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem
+set path=.;%JAVA_HOME%\bin;%GRADLE_HOME%\bin;%MAVEN_HOME%\bin;%SVN_HOME%\bin;%MYSQL_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem
 
