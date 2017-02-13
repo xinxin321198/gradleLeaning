6a5810cd27ec211fbc2cd714de9a5d6b09e68f9e
loserStar <362527240@qq.com>
2017-02-13 16:08:47 +0800

    junit

---
+++ gradleWebProject/src/test/java/com/loserstar/gradletest/HelloLoserStarTest.java
@@ -0,0 +1,14 @@
+package com.loserstar.gradletest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloLoserStarTest {
+
+	@Test
+	public void test() {
+		System.out.println("testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest");
+	}
+
+}
